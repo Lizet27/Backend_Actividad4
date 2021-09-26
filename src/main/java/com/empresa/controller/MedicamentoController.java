9ff5efb0b2e2cf102f78238443e6c63821d14d6b
ldiana27.ldor <ldiana27.ldor@gmail.com>
2021-09-25 22:41:03 -0500

    continuar

--- src/main/java/com/empresa/controller/MedicamentoController.java
+++ src/main/java/com/empresa/controller/MedicamentoController.java
@@ -1,9 +1,12 @@
 package com.empresa.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,9 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.empresa.entity.Medicamento;
 import com.empresa.service.MedicamentoService;
+import com.empresa.util.Constantes;
 
 @RestController
 @RequestMapping("/rest/medicamento")
+//@CrossOrigin(origins = "http:/localhost:42100")
 public class MedicamentoController {
 
 	@Autowired
@@ -30,8 +35,23 @@ public class MedicamentoController {
 	
 	@PostMapping
 	@ResponseBody
-	public ResponseEntity<Medicamento> inserta (@RequestBody Medicamento obj){
-	Medicamento objSalida = service.insertaActualiza(obj);
-	return ResponseEntity.ok(objSalida);
+	public ResponseEntity<Map<String, Object>> inserta (@RequestBody Medicamento obj){
+		Map<String, Object> Salida = new HashMap<>();
+		try {
+			Medicamento objSalida = service.insertaActualiza(obj);
+			if (objSalida ==null) {
+				Salida.put("mensaje", Constantes.MENSAJE_REG_ERROR);
+				
+			} else {
+				Salida.put("mensaje", Constantes.MENSAJE_REG_EXITOSO);
+
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			Salida.put("mensaje", Constantes.MENSAJE_REG_ERROR);
+		}
+	
+	return ResponseEntity.ok(Salida);
 	}
 }
